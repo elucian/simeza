055ce394a808d50f812cf632c5d410b294abe0ba
Elucian Moise <elucian.moise@gmail.com>
2026-09-07 06:11:08 -0400

    fix errors in publish

--- run.sh
+++ run.sh
@@ -128,12 +128,14 @@ elif [ "$CMD" == "build" ]; then
 elif [ "$CMD" == "publish" ]; then
     echo "Publishing..."
     python script/release.py
-    CANDIDATE_VERSION=$(python -c "import json; print(json.load(open("release/releases.json"))["candidate"]["version"])")
+    
+    CANDIDATE_VERSION=$(python -c "import json; print(json.load(open('release/releases.json'))['candidate']['version'])")
+    
     if [ -z "$CANDIDATE_VERSION" ]; then
         echo "No release candidate found. Nothing to commit or push."
     else
         git add .
-        VERSION=$(python -c "import json; print(json.load(open("release/releases.json"))["published"]["version"])")
+        VERSION=$(python -c "import json; print(json.load(open('release/releases.json'))['published']['version'])")
         git commit -m "Publish release: v$VERSION"
         git tag "v$VERSION"
         git push origin main "v$VERSION"
